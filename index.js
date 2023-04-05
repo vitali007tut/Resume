a9d9609604c7e7d88e0eb1d01c42710b66f955d9
vitali007tut <vitali007@tut.by>
2023-04-05 22:52:35 +0300

    feat: background change

--- index.js
+++ index.js
@@ -7,14 +7,18 @@ function changeColor1() {
     document.body.style.background = ''
     document.body.style.color = ''
     moon.style.opacity = 1
+    moon.style.width = '30px'
     sun.style.opacity = 0
+    sun.style.width = 0
 }
 
 moon.addEventListener('click', changeColor2)
 
 function changeColor2() {
-    document.body.style.background = 'radial-gradient(100% 215.42% at 0% 0%, #5B483A 0%, #262425 100%)'
+    document.body.style.background = 'linear-gradient(#5B483A, #262425)'
     document.body.style.color = 'white'
     sun.style.opacity = 1
+    sun.style.width = '30px'
     moon.style.opacity = 0   
+    moon.style.width = 0
 }
