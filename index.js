f981c57f9dacb5a5b30b8c8a0c7315f5631f8bc6
vitali007tut <vitali007@tut.by>
2023-06-17 23:25:06 +0300

    feat: add link project

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const sun = document.querySelector('.sun');
 const moon = document.querySelector('.moon');
 const container = document.querySelector('.container');
 const links = document.querySelectorAll('a');
-const githubIco = document.querySelector('.github-account')
+const githubIco = document.querySelector('.github-account');
+const sectionTitle = document.querySelectorAll('.section-title');
 
 
 sun.addEventListener('click', changeColor1)
@@ -11,7 +12,8 @@ function changeColor1() {
     container.classList.remove('container-dark');
     links.forEach(e => e.classList.remove('dark-mode'));
     githubIco.classList.remove('dark-mode');
-    document.body.style.color = ''
+    sectionTitle.forEach(element => element.classList.remove('dark-mode'));
+    document.body.style.color = '';
     moon.style.opacity = 1
     moon.style.width = '30px'
     sun.style.opacity = 0
@@ -24,7 +26,8 @@ function changeColor2() {
     container.classList.add('container-dark');
     links.forEach(e => e.classList.add('dark-mode'));
     githubIco.classList.add('dark-mode');
-    document.body.style.color = 'white'
+    document.body.style.color = 'white';
+    sectionTitle.forEach(element => element.classList.add('dark-mode'));
     sun.style.opacity = 1
     sun.style.width = '30px'
     moon.style.opacity = 0   
